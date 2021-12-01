734b8124e6198d7aeb3bdcd04db09ff05c1adf16
Anita Deszczynska <anitadeszczynska@gmail.com>
2021-12-01 10:13:40 +0100

    Add player choices

--- js/script.js
+++ js/script.js
@@ -13,7 +13,24 @@ if(randomNumber == 1){
           computerove = 'nozyce';
 }
 
-    
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+    else if (playerInput=='2'){
+        playerMove = 'papier';
+    }
+
+    else if (playerInput=='3'){
+        playerMove = 'nozyce'
+    }
+
+printMessage('Twój ruch to: ' + playerMove);    
 
   
 
