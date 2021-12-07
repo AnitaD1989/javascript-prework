0ff82afc422520934c5173368621d5f26e1d0790
Anita Deszczynska <anitadeszczynska@gmail.com>
2021-12-07 10:29:24 +0100

    Change the code and add the score

--- js/script.js
+++ js/script.js
@@ -1,57 +1,10 @@
 
-{
-function playGame (playerInput) { 
-    const getMoveName= function (choiceNumber) {
-        if(choiceNumber == 1) return 'kamien';
-        if(choiceNumber == 2) return 'papier';
-        if(choiceNumber == 3) return 'nozyce';
-    }
+let playerScore = 0;
+let computerScore = 0;
 
-    const displayResult= function(argComputerMove, argPlayerMove) {
-        if ((argComputerMove == 'kamien' && argPlayerMove == 'papier') || 
-            (argComputerMove == 'papier' && argPlayerMove == 'nozyce') || 
-            (argComputerMove == 'nozyce' && argPlayerMove == 'kamien')) {
-            printMessage('Ty wygrywasz!');
-        }
-        else if (argComputerMove == argPlayerMove) {
-            printMessage('Remis!');
-        }
-        else {
-            printMessage('Ty przegrałeś!');
-        }
-    }
-   
-    // czyścimy stare komunikaty z HTML-a
-    clearMessages();
+function playGame(playerInput) { 
 
-    // losujemy wybór gracza (w formie liczbowej) np. 1, 2, 3
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-    // konwertujemy wybory gracza i komputera z liczb na zapis tekstowy, czyli np. 1 -> kamien
-    const playerMove = getMoveName(playerInput); 
-    const computerMove = getMoveName(randomNumber);
-
-    // sprawdż kto wygrał i zapisz tę informację w HTML-u
-    displayResult(playerMove, computerMove);
-}
-
-document.getElementById('play-rock').addEventListener('click', function () {
-    playGame(1);
-});
-
-document.getElementById('play-paper').addEventListener('click', function () {
-    playGame(2);
-});
-
-document.getElementById('play-scissors').addEventListener('click', function () {
-    playGame(3);
-});
-
-let playerScore, computerScore = 0;
-
-function playGame (playerInput) { 
-
-    const getMoveName= function (choiceNumber) {
+    function getMoveName(choiceNumber) {
         if(choiceNumber == 1) return 'kamien';
         if(choiceNumber == 2) return 'papier';
         if(choiceNumber == 3) return 'nozyce';
@@ -101,4 +54,4 @@ document.getElementById('play-scissors').addEventListener('click', function () {
 });
 
 
-}
+
